26aec843b2c9ac68e8a508b9942162fc5e52083f
yuncong-SH-Office <yuncong@SH-PC>
2026-01-21 15:03:00 +0800

    (feat)[2026-01-21] 更新PayLaterMessaging的部分

---
+++ js/data.js
@@ -0,0 +1,85 @@
+export const navData = [
+    {
+        title: "JS V6",
+        icon: "menu",
+        groups: [
+            {
+                title: "Main Test Site",
+                icon: "folder",
+                items: [
+                    {
+                        description: "Test Case Collection Please Visit:",
+                        url: "https://jsv6-test.pages.dev/",
+                        text: "https://jsv6-test.pages.dev/",
+                    },
+                ],
+            },
+            {
+                title: "Detailed Feature",
+                icon: "folder",
+                items: [
+                    {
+                        description: "JSv6-JSv5 Talking point Comparison:",
+                        url: "jsv6-test/jsv6-jsv5-comparison/jsv6-jsv5-comparison.html",
+                        text: "jsv6-jsv5-comparison.html",
+                    },
+                    {
+                        description: "Cross-Border-Messaging:",
+                        url: "PayLater/JSV6-PLM/jsv6-cross-border-msg.html",
+                        text: "jsv6-cross-border-msg",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        title: "JS V5",
+        icon: "menu",
+        groups: [
+            {
+                title: "Main Test Site",
+                icon: "folder",
+                items: [
+                    {
+                        description: "Test Env下的GMS Exhibition导览入口:",
+                        url: "https://gmsshanghai.github.io/",
+                        text: "https://gmsshanghai.github.io/",
+                    },
+                ],
+            },
+            {
+                title: "Smart Payment Button",
+                icon: "folder",
+                items: [
+                    {
+                        description: "BCDC的各种常见情况:",
+                        url: "BCDC",
+                        text: "查看使用BCDC时的正确和错误做法(已经添加自动展开参数)",
+                    },
+                    {
+                        description:
+                            "Vault和Buy Now Pay Later冲突问题(2024-06):",
+                        url: "vault-BNPL-conflict",
+                        text: "查看conflict",
+                    },
+                ],
+            },
+            {
+                title: "ACDC",
+                icon: "folder",
+                items: [
+                    {
+                        description: "ApplePay详细测试Demo:",
+                        url: "applePay/applepay-test.html",
+                        text: "ApplePay Test",
+                    },
+                    {
+                        description: "ACDC First View, 记得打开控制台:",
+                        url: "ACDC/ACDC-Sample.html",
+                        text: "查看ACDC",
+                    },
+                ],
+            },
+        ],
+    },
+];
