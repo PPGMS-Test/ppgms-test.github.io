cdd495769aef3505873ba87d2ecd01dd1e29ee67
tigerdahuacao <computer_email@yeah.net>
2026-05-13 17:22:31 +0800

    fix(paypal-backend-api)[2026-05-13]: 绕过 swagger-ui-react 与 React 19 类型不兼容问题

--- paypal-backend-api/src/app/page.tsx
+++ paypal-backend-api/src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+// @ts-ignore - swagger-ui-react types incompatible with React 19
 import SwaggerUI from 'swagger-ui-react'
 import 'swagger-ui-react/swagger-ui.css'
 
 export default function Home() {
+  // @ts-ignore
   return <SwaggerUI url="/openapi.json" />
 }
